ccf3105f768fca6eca907c20b4d8e4306e266f39
Dave <dlrobjr@gmail.com>
2023-11-16 15:44:56 -0700

    cleaned up some more

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //                                    PART 1
 
 // button variables
-let stopButton = document.getElementById("stopButton")
-let slowButton = document.getElementById("slowButton")
-let goButton = document.getElementById("goButton")
+let stopButton = document.getElementById("stopButton");
+let slowButton = document.getElementById("slowButton");
+let goButton = document.getElementById("goButton");
 
 
 // Function turning off and on each class type
@@ -20,26 +20,26 @@ slowButton.addEventListener('click', function (){
   lightFunction("slowLight", "slow");
 });
 goButton.addEventListener('click', function (){
-  lightFunction("goLight", "go")
+  lightFunction("goLight", "go");
 });
 
 //                                  PART 2
 
 // function printing entered button to the console
 var mouseEnter = function(event){
-  console.log(`Entered ${event.target.textContent} button`)
+  console.log(`Entered ${event.target.textContent} button`);
 }
 
 // function printing left button to the console
 var mouseLeft = function(event){
-  console.log(`Left ${event.target.textContent} button`)
+  console.log(`Left ${event.target.textContent} button`);
 }
 
 // creating event listeners using functions made above
 var buttonArr = [stopButton, slowButton, goButton];
 buttonArr.forEach((element) => {
-  element.addEventListener('mouseenter', mouseEnter)
-  element.addEventListener('mouseleave', mouseLeft)
+  element.addEventListener('mouseenter', mouseEnter);
+  element.addEventListener('mouseleave', mouseLeft);
 });
 
 
